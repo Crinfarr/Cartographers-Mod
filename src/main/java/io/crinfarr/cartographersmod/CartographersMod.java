fb9457bfcba514f19d36c93f5373d3bb475aecbf
Crinfarr <dev@crinfarr.io>
2024-07-30 23:29:13 -0500

    it works now

---
+++ src/main/java/io/crinfarr/cartographersmod/CartographersMod.java
@@ -0,0 +1,75 @@
+package io.crinfarr.cartographersmod;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.Stack;
+
+@Mod(CartographersMod.MODID)
+public class CartographersMod {
+    public static final String MODID = "cartographersmod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public CartographersMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::postCommonSetup);
+
+
+        MinecraftForge.EVENT_BUS.register(modEventBus);
+    }
+
+    private void postCommonSetup(final FMLClientSetupEvent _event) {
+        LOGGER.info("Entering postCommonSetup");
+        @NotNull Set<Map.Entry<ResourceKey<Item>, Item>> items = ForgeRegistries.ITEMS.getEntries();
+        /**
+         * DEBUG
+         * items.forEach(resourceKeyItemEntry -> {
+         *      LOGGER.debug("Found {}", resourceKeyItemEntry.getKey().location());
+         * });
+         */
+        final Stack<String> itemStack = new Stack<>();
+        items.forEach(item -> {
+            itemStack.push(
+                    String.format(
+                            "%c%s",
+                            item
+                                    .getKey()
+                                    .location()
+                                    .toString()
+                                    .length(),
+                            item
+                                    .getKey()
+                                    .location()
+                    )
+            );
+        });
+        final String outString = String.join("", itemStack);
+        try (FileWriter fileWriter = new FileWriter("cartographyMap.dump");) {
+            fileWriter.write(outString);
+            LOGGER.info("Dumped {} items", items.size());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
